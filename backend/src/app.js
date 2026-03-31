c3e4b7b11824c42c49a93239e7712ca5c2ef26e0
JustAjibola <akinsola51@gmail.com>
2026-03-31 14:16:14 +0100

    progress

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,13 +2,16 @@ import express from "express";
 
 //  routes import
 import userRouter from "./routes/user.route.js";
+import postRouter from "./routes/post.route.js";
 
 const app = express(); // Create an Express application
 
 app.use(express.json()); // Middleware to parse JSON request bodies
 
+
 // routes declaration
 app.use("/api/v1/users", userRouter);
+app.use("/api/v1/posts", postRouter);
 
 // example route: http://localhost:8000/api/v1/users/register
 
